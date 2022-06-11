5b2b707e0ad94b45bdba5fe74778f034746c2a48
leeyongwoo93 <rmstjd23@gmail.com>
2022-06-11 16:16:41 +0900

    vector add iterator

--- Project1/include/data_structure/Vector.h
+++ Project1/include/data_structure/Vector.h
@@ -7,17 +7,116 @@ namespace _DataStructures_
 {
 	template <typename V>
 	class Vector {
-		
+	public:
+		class iterator {
+
+
+		public:
+			iterator() :ptr(nullptr) {}
+			iterator(const iterator& it) :ptr(it.ptr) {}
+			~iterator() {}
+
+
+			bool operator ==(const iterator& it) {
+
+				return ptr == it.ptr;
+			}
+
+			bool operator !=(const iterator& it) {
+
+				return ptr != it.ptr;
+			}
+
+			iterator& operator =(const iterator& it) {
+
+				ptr = it.ptr;
+
+				return *this;
+			}
+
+			iterator operator +(const int n) {
+				iterator ret(*this);
+
+				for (int i = 0; i < n; i++)
+					ret++;
+
+				return ret;
+			}
+			iterator& operator ++() {
+
+				ptr++;
+
+				return *this;
+			}
+			iterator& operator ++(int) {
+				iterator ret(*this);
+
+				ptr++;
+
+				return ret;
+			}
+			iterator& operator +=(const int n) {
+
+				for (int i = 0; i < n; i++)
+					ptr++;
+
+				return *this;
+			}
+
+
+			iterator operator -(const int n) {
+				iterator ret(*this);
+
+				for (int i = 0; i < n; i++)
+					ret--;
+
+				return ret;
+			}
+			int operator -(iterator it) {
+				
+				return (ptr - it.ptr);
+			}
+
+			iterator& operator --() {
+
+				ptr--;
+
+				return *this;
+			}
+			iterator& operator --(int) {
+				iterator ret(*this);
+
+				ptr--;
+
+				return ret;
+			}
+			iterator& operator -=(const int n) {
+
+				for (int i = 0; i < n; i++)
+					ptr--;
+
+				return *this;
+			}
+
+			V& operator *() {
+
+				return *ptr;
+			}
+
+		public:
+			V* ptr;
+		};
+
 	public:
 		Vector()
 		{
-			ptr = (V*)malloc(sizeof(V) * 1024);
+			ptr = new V[1024];//(V*)malloc(sizeof(V) * 1024);
 			itemCount = 0;
 			maxSize = 1024;
 		}
 		Vector(unsigned int count, const V& value)
 		{
-			ptr = (V*)malloc(sizeof(V) * count * 2);
+			ptr = new V[count * 2];//(V*)malloc(sizeof(V) * count * 2);
 			for (unsigned int i = 0; i < count; i++)
 				memcpy_s(ptr + i, sizeof(V), &value, sizeof(V));
 
@@ -26,7 +125,7 @@ namespace _DataStructures_
 		}
 		~Vector()
 		{
-			free(ptr);
+			delete[] ptr;
 		}
 
 		bool empty()
@@ -51,21 +150,109 @@ namespace _DataStructures_
 			if(itemCount > 0)
 				itemCount--;
 		}
+		void insert(iterator index, V value)
+		{
+			if (itemCount + 1 > maxSize)
+				resize(maxSize * 2);
+
+			for (iterator it = end(); it != index; it--)
+				*it = *(it - 1);
+
+			*index = value;
+			itemCount++;
+		}
+
+		void insert(iterator index, unsigned int count, V value)
+		{
+			while (itemCount + count > maxSize)
+				resize(maxSize * 2);
+
+			for (iterator it = rbegin() - count; it != index; it--)
+				*it = *(it - count);
+
+			for (int i = 0; i < count; i++)
+			{
+				*index = value;
+				index++;
+			}
+
+			itemCount += count;
+		}
+
+		void insert(iterator index, iterator start, iterator end)
+		{
+			int count = end - start;
+
+			while (itemCount + count > maxSize)
+				resize(maxSize * 2);
+
+			for (iterator it = rbegin() - count; it != index; it--)
+				*it = *(it - count);
+
+			for (iterator it = start; it != end; it++)
+			{
+				*index = *it;
+				index++;
+			}
+
+			itemCount += count;
+		}
+
+		iterator erase(iterator index)
+		{
+			for (iterator it = index; it != end() - 1; it++)
+				*it = *(it + 1);
+			
+			itemCount--;
+
+			return index;
+		}
+
+		iterator erase(iterator _start, iterator _end)
+		{
+			int count = _end - _start;
+
+			for (iterator it = _start; it != end() - count; it++)
+				*it = *(it + count);
+
+			itemCount -= count;
+
+			return _start;
+		}
+
 		V& front()
 		{
 			return ptr[0];
 		}
-		V& front()
+		V& back()
 		{
 			return ptr[itemCount - 1];
 		}
-		V* begin()
+
+		iterator begin()
 		{
-			return ptr;
+			_iterator.ptr = ptr;
+
+			return _iterator;
 		}
-		V* end()
+		iterator end()
 		{
-			return ptr + itemCount;
+			_iterator.ptr = ptr + itemCount;
+
+			return _iterator;
+		}
+
+		iterator rbegin()
+		{
+			_iterator.ptr = ptr + (itemCount - 1);
+
+			return _iterator;
+		}
+		iterator rend()
+		{
+			_iterator.ptr = ptr - 1;
+
+			return _iterator;
 		}
 
 		V& at(unsigned int index)
@@ -76,20 +263,20 @@ namespace _DataStructures_
 		{
 			return *(ptr + index);
 		}
+
 	private:
 		void resize(unsigned int size)
 		{
-			V* temp = (V*)malloc(sizeof(V) * size);
+			V* temp = new V[size];// (V*)malloc(sizeof(V) * size);
 
 			memcpy_s(temp, sizeof(V) * size, ptr, sizeof(V) * itemCount);
-			free(ptr);
+			delete[] ptr;
 
 			ptr = temp;
 			maxSize = size;
 			if (itemCount > size)
 				itemCount = size;
 		}
-
 	public:
 
 		unsigned int maxSize;
@@ -97,6 +284,7 @@ namespace _DataStructures_
 
 	private:
 		V* ptr;
+		iterator _iterator;
 
 		unsigned int itemCount;
 		
--- Project1/src/main.cpp
+++ Project1/src/main.cpp
@@ -5,6 +5,7 @@
 #include "../include/data_structure/Deque.h"
 #include "../include/data_structure/BinaryTree.h"
 #include "../include/data_structure/Vector.h"
+#include "../include/data_structure/HashTable.h"
 
 using namespace _DataStructures_;
 using namespace std;
@@ -36,6 +37,7 @@ void treeTest()
 		{
 			upTree.merge(&downTree, upTree.compare);
 		}
+
 		cout << endl;
 		cout << "  upTree" << endl;
 		upTree.printTree();
@@ -49,7 +51,7 @@ void vectorTest()
 {
 	Vector<double> v1;
 	Vector<double> v2(100, 10.0);
-	for (int i = 0; i < 100000; i++)
+	for (int i = 0; i < 100; i++)
 	{
 		v1.push_back((double)i);
 		v2.push_back((double)i);
@@ -58,12 +60,157 @@ void vectorTest()
 		cout << "v1 size : " << v1.size() << ", maxSize : " << v1.maxSize << ", last : " << v1[v1.size() - 1] << "\t";
 		cout << "v2 size : " << v2.size() << ", maxSize : " << v2.maxSize << ", last : " << v2[v2.size() - 1] << endl;
 	}
+
+	for (int i = 0; i < v1.size(); i++)
+	{
+		cout << v1[i] << "\t";
+	}
+	cout << endl << endl;
+	v1.insert(v1.begin() + 1, 22);
+	for (int i = 0; i < v1.size(); i++)
+	{
+		cout << v1[i] << "\t";
+	}
+	cout << endl << endl;
+	v1.erase(v1.begin() + 4);
+	for (int i = 0; i < v1.size(); i++)
+	{
+		cout << v1[i] << "\t";
+	}
+	cout << endl << endl;
+
+	v1.erase(v1.begin() + 10, v1.end() - 10);
+	for (int i = 0; i < v1.size(); i++)
+	{
+		cout << v1[i] << "\t";
+	}
+	cout << endl << endl;
+
+	for (auto it : v1)
+	{
+		cout << it;
+	}
+
+
 }
 
+void hashTest()
+{
+	HashTable<string, int> htb;
+
+	cout << sizeof("123") << endl;
+
+	htb.insert("name0", 10);
+	cout << htb.size() << endl;
+	htb.insert("name1", 11);
+	htb.insert("name2", 12);
+	cout << htb["name0"] << endl;
+	cout << htb["name1"] << endl;
+	cout << htb["name2"] << endl;
+
+	//HashTable<string, int> htb2(2);
+
+	//htb2.insert("name0", 10);
+	//htb2.insert("name1", 11);
+	//htb2.insert("name2", 12);
+
+	//cout << htb2["name0"] << endl;
+	//cout << htb2["name1"] << endl;
+	//cout << htb2["name2"] << endl;
+
+
+	//HashTable<int, int> htb;
+	//htb.insert(0, 10);
+	//htb.insert(1, 11);
+	//htb.insert(2, 12);
+	//htb[5] = 15;
+	//cout << htb[0] << endl;
+	//cout << htb[1] << endl;
+	//cout << htb[2] << endl;
+	//cout << htb[5] << endl;
+
+	//HashTable<int, int> htb2(2);
+	//htb2.insert(0, 10);
+	//htb2.insert(1, 11);
+	//htb2.insert(2, 12);
+	//cout << htb2[0] << endl;
+	//cout << htb2[1] << endl;
+	//cout << htb2[2] << endl;
+
+	
+}
+
+void LLtest()
+{
+	LinkedList<int> tl;
+
+	for (int i = 0; i < 9; i++)
+	{
+		tl.push_front(i);
+		tl.insert(tl.end(), i);
+		tl.insert(tl.begin() + 2, i);
+
+	}
+
+	for (auto item : tl)
+		cout << " " << item;
+	cout << endl;
+
+	LinkedList<int> tl2(10, 9);
+	for (auto item : tl2)
+		cout << " " << item;
+	cout << endl;
+
+	tl.insert(tl.begin(), tl2);
+
+	for (auto item : tl)
+		cout << " " << item;
+	cout << endl;
+
+	tl.insert(tl.end(), tl2);
+	for (auto item : tl)
+		cout << " " << item;
+	cout << endl;
+
+	cout << endl << endl;
+
+	tl.erase(tl.begin());
+	for (auto item : tl)
+		cout << " " << item;
+	cout << endl;
+
+	tl.erase(tl.begin()+ 10);
+	for (auto item : tl)
+		cout << " " << item;
+	cout << endl;
+
+	tl.erase(tl.begin(), tl.begin() + 20);
+	for (auto item : tl)
+		cout << " " << item;
+	cout << endl;
+
+	cout << endl << endl;
+
+	tl.insert(tl.begin(), tl2.begin() + 1, tl2.begin() + 6);
+	for (auto item : tl)
+		cout << " " << item;
+	cout << endl;
+	tl.insert(tl.end(), tl2.begin() + 1, tl2.begin() + 6);
+	for (auto item : tl)
+		cout << " " << item;
+	cout << endl;
+}
 
 void main()
 {
-	//treeTest();
+	vector<int> test;
 
+	test.push_back(1);
+	test.push_back(2);
+	test.push_back(3);
+
+	//LLtest();
+	//treeTest();
 	vectorTest();
+	//hashTest();
 }
